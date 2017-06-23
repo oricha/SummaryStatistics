1b24f65bca4aba785bd5de2f182d4813719d2791
Karel Muñiz Perdomo <oricha@gmail.com>
2017-06-23 23:05:25 +0200

    Added initial project

---
+++ src/main/java/com/example/SummaryStatistics/SummaryStatisticsApplication.java
@@ -0,0 +1,12 @@
+package com.example.SummaryStatistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryStatisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaryStatisticsApplication.class, args);
+	}
+}
